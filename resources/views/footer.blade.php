99c6a1b1164fee75fd1b19de5571585add56b79e
greg <gregoiredalmasso@gmail.com>
2019-10-19 14:23:11 +0200

    first commit

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,9 @@
+<!-- Footer -->
+<footer class="page-footer font-small">
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© 2018 Copyright:
+    <p>Coen Bros 2019</p>
+  </div>
+
+</footer>
