866e7997e1c816fc93e54d12187f1f310b558f26
greg-Da <gregoiredalmasso@gmail.com>
2019-10-21 00:08:01 +0200

    update navbar

--- resources/views/component/nav.blade.php
+++ resources/views/component/nav.blade.php
@@ -1,16 +1,24 @@
 <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
   <ul class="navbar-nav">
     <li class="nav-item active">
-      <a class="nav-link" href="#">Active</a>
+      <a class="nav-link" href="#">Accueil</a>
     </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">Link</a>
+    <li class="nav-item active">
+      <a class="nav-link" href="#">Movies</a>
     </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">Link</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link disabled" href="#">Disabled</a>
-    </li>
+    <li class="nav-item" active>
+      
+
+<div class="dropdown show">
+  <a class="nav-link dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Coen Bros
+  </a>
+
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+    <a class="dropdown-item" href="#">Ethan</a>
+    <a class="dropdown-item" href="#">Joel</a>
+  </div>
+</div>
   </ul>
+  <input type="text" placeholder="Search.." class="searchbar ml-auto">
 </nav>
